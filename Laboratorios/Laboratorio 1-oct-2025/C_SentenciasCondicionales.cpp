eebd2bcfeb7878af67df2ac96c1e18054d4e4de3
alejDev <aseckrr05@gmail.com>
2025-10-23 17:49:03 +0200

    lab 1-oct-2025

---
+++ Laboratorios/Laboratorio 1-oct-2025/C_SentenciasCondicionales.cpp
@@ -0,0 +1,16 @@
+/**
+ * Calculadora de sumar y restar
+Desarrolla un código en C++ que implemente una calculadora sencilla que solo permite sumar y restar.
+El programa permitirá elegir la operación a realizar mediante un menú sencillo como el siguiente:
+1 – Sumar
+2 – Restar
+En el caso de que el usuario teclee otro número distinto a 1 ó 2, el programa mostrará un mensaje de error.
+Los operandos se pedirán por teclado y, según la operación seleccionada por el usuario, se mostrará el
+resultado en pantalla.
+El algoritmo debe estar optimizado: no repetir código.
+ */
+#include <iostream>
+using namespace std;
+int main(){
+    return 0;
+}
